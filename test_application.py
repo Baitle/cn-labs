ac7dcdcecf1456df70912b6427209dae934e5254
Daniel Dias <daniel.jose.dias@outlook.com>
2022-03-02 16:02:41 +0000

    renames file for unit testing

--- test_application.py
+++ test_application.py
@@ -1,5 +1,5 @@
-from server import server
-with server.test_client() as c:
+from application import application
+with application.test_client() as c:
     response = c.get('/')
     assert response.data == b'Hello World!'
     assert response.status_code == 200
